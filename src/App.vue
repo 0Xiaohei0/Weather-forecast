a10434325d27613809c137954462d0d3cdd1db10
0Xiaohei0 <15852786007ysc@gmail.com>
2021-09-10 13:01:07 -0400

    retrieve data

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <weather />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import weather from "./components/weather.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: { weather },
+  name: "App",
+  data() {
+    return {
+      weatherData: "",
+    };
+  },
+  created() {
+    const api =
+      "https://api.open-meteo.com/v1/forecast?latitude=43.48&longitude=-79.64&hourly=temperature_2m,weathercode";
+    fetch(api)
+      .then((resp) => resp.json())
+      .then((data) => (this.weatherData = data))
+      .catch((err) => console.log(err.message));
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/weather.vue
@@ -0,0 +1,7 @@
+<template> <h1>weather component</h1> </template>
+
+<script>
+export default {
+  name: "weather",
+};
+</script>
