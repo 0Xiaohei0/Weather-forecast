46ddab330a77a06b70e9c7325a8c4c98ca6fad19
0Xiaohei0 <15852786007ysc@gmail.com>
2021-09-10 21:33:53 -0400

    week forecast

--- src/components/weekForecast.vue
+++ src/components/weekForecast.vue
@@ -1,44 +1,8 @@
 <template>
   <div class="container">
-    <div class="dayItem">
-      <p class="dayOfTheWeek">Today</p>
-      <font-awesome-icon class="fa-2x" icon="sun" />
-      <p class="high">21 &#8451;</p>
-      <p class="low">16 &#8451;</p>
-    </div>
-    <div class="dayItem">
-      <p class="dayOfTheWeek">Today</p>
-      <font-awesome-icon class="fa-2x" icon="sun" />
-      <p class="high">21 &#8451;</p>
-      <p class="low">16 &#8451;</p>
-    </div>
-    <div class="dayItem">
-      <p class="dayOfTheWeek">Today</p>
-      <font-awesome-icon class="fa-2x" icon="sun" />
-      <p class="high">21 &#8451;</p>
-      <p class="low">16 &#8451;</p>
-    </div>
-    <div class="dayItem">
-      <p class="dayOfTheWeek">Today</p>
-      <font-awesome-icon class="fa-2x" icon="sun" />
-      <p class="high">21 &#8451;</p>
-      <p class="low">16 &#8451;</p>
-    </div>
-    <div class="dayItem">
-      <p class="dayOfTheWeek">Today</p>
-      <font-awesome-icon class="fa-2x" icon="sun" />
-      <p class="high">21 &#8451;</p>
-      <p class="low">16 &#8451;</p>
-    </div>
-    <div class="dayItem">
-      <p class="dayOfTheWeek">Today</p>
-      <font-awesome-icon class="fa-2x" icon="sun" />
-      <p class="high">21 &#8451;</p>
-      <p class="low">16 &#8451;</p>
-    </div>
-    <div class="dayItem">
-      <p class="dayOfTheWeek">Today</p>
-      <font-awesome-icon class="fa-2x" icon="sun" />
+    <div v-for="(d, index) in displayData" :key="index" class="dayItem">
+      <p class="dayOfTheWeek">{{ d.day }}</p>
+      <font-awesome-icon class="fa-2x" :icon="d.weatherIcon" />
       <p class="high">21 &#8451;</p>
       <p class="low">16 &#8451;</p>
     </div>
@@ -46,11 +10,30 @@
 </template>
 
 <script>
-import { getNext7Days } from "./utility/util";
+import { getNext7Days, WMOtoIcon } from "./utility/util";
 export default {
+  data() {
+    return {
+      displayData: [],
+    };
+  },
   props: ["data"],
+  methods: {
+    loadData() {
+      let day = getNext7Days(this.data);
+      let weather = [];
+      //let lowTemp = [];
+      //let highTemp = [];
+      for (let i = 0; i < 168; i += 24) {
+        weather.push(WMOtoIcon(this.data.hourly.weathercode[i]));
+      }
+      for (let i = 0; i < weather.length; i++) {
+        this.displayData.push({ day: day[i], weatherIcon: weather[i] });
+      }
+    },
+  },
   mounted() {
-    console.log(getNext7Days(this.data));
+    this.loadData();
   },
 };
 </script>
